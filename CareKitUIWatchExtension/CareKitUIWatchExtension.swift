130d953404fe51e8c6f60b51fede09457bbe4c74
Elizaveta <dyuchkovae@gmail.com>
2020-05-02 17:37:23 +0200

    Initial Commit

---
+++ CareKitUIWatchExtension/CareKitUIWatchExtension.swift
@@ -0,0 +1,11 @@
+//
+//  CareKitUIWatchExtension.swift
+//  CareKitUIWatchExtension
+//
+//  Created by Elizaveta Diuchkova on 02/05/2020.
+//  Copyright © 2020 Elizaveta Diuchkova. All rights reserved.
+//
+
+class CareKitUIWatchExtension {
+
+}
