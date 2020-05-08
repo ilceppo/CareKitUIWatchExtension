de4d6a561eb6437f5b4f36f381d9d76aea9b2d8f
Matteo Cioppa <matteo@cioppa.me>
2020-05-08 13:17:08 +0200

    Added missing example files

---
+++ Example/ExampleApp/ExampleApp WatchKit Extension/CareKitUI/LogTaskView.swift
@@ -0,0 +1,32 @@
+//
+//  LogTaskView.swift
+//  ExampleApp WatchKit Extension
+//
+//  Created by Matteo Cioppa on 07/05/2020.
+//  Copyright © 2020 Matteo Cioppa. All rights reserved.
+//
+
+import SwiftUI
+import CareKitUIWatchExtension
+
+struct LogTaskView: View {
+    @State var events: [String] = []
+    
+    var body: some View {
+        
+        ScrollView {
+            ButtonLog(instructions: Text("..."), header: Text("..."), footer: nil, action: {
+                print("a")
+                let date = Date().getFormattedDate(format: "HH:mm")
+                self.events.append(date.description)
+            }, details: events)
+        }
+        
+    }
+}
+
+struct LogTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogTaskView()
+    }
+}
---
+++ Example/ExampleApp/ExampleApp WatchKit Extension/DateFormat+Date.swift
@@ -0,0 +1,17 @@
+//
+//  DateFormat+Date.swift
+//  ExampleApp WatchKit Extension
+//
+//  Created by Matteo Cioppa on 07/05/2020.
+//  Copyright © 2020 Matteo Cioppa. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
